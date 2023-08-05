4f99283523ee07e8a609452340c2cb5a08f343f4
Egor Slobodskoy <egoreast.true@gmail.com>
2023-08-05 13:31:01 +0300

    updated project structure

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+/* eslint-disable */
+const process = require('process');
+
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    '@feature-sliced',
+  ],
+  parser: '@typescript-eslint/parser',
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+  rules: {
+    'import/no-internal-modules': [
+      'warn',
+      {
+        allow: ['**/ui/*', 'widgets/*', 'features/*', 'entities/*', 'shared/*'],
+      },
+    ],
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    curly: ['error', 'all'],
+    'arrow-body-style': ['error', 'as-needed'],
+  },
+};
