2b471832fd318e764445bac23ade859f5f12448f
Egor Slobodskoy <egoreast.true@gmail.com>
2023-09-20 23:59:36 +0300

    changed styles. Removed unnecessary components. Added styled-components package

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import './globals.css';
 import type { Metadata } from 'next';
-import { Inter, Montserrat } from 'next/font/google';
+import { Montserrat } from 'next/font/google';
 
 const inter = Montserrat({ subsets: ['latin', 'cyrillic'] });
 
 export const metadata: Metadata = {
   title: 'Академия Слободских',
-  description: '',
+  description: 'My App is a...',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
